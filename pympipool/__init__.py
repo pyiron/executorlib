5b12ab0163a7a71050e1dc612c57f8da567aa196
Jan Janssen <janssen@lanl.gov>
2023-05-25 13:12:31 -0600

    Move `_pool_open()` to `__init__()`

--- pympipool/__init__.py
+++ pympipool/__init__.py
@@ -42,8 +42,43 @@ class Pool(object):
         self._context = None
         self._enable_flux_backend = enable_flux_backend
         self._oversubscribe = oversubscribe
+        self._pool_open()
 
     def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._pool_close()
+
+    def map(self, function, lst):
+        """
+        Map a given function on a list of attributes.
+
+        Args:
+            function: function to be applied to each element of the following list
+            lst (list): list of arguments the function should be applied on
+
+        Returns:
+            list: list of output generated from applying the function on the list of arguments
+        """
+        # Cloudpickle can either pickle by value or pickle by reference. The functions which are communicated have to
+        # be pickled by value rather than by reference, so the module which calls the map function is pickled by value.
+        # https://github.com/cloudpipe/cloudpickle#overriding-pickles-serialization-mechanism-for-importable-constructs
+        # inspect can help to find the module which is calling pympipool
+        # https://docs.python.org/3/library/inspect.html
+        # to learn more about inspect another good read is:
+        # http://pymotw.com/2/inspect/index.html#module-inspect
+        # 1 refers to 1 level higher than the map function
+        try:  # When executed in a jupyter notebook this can cause a ValueError - in this case we just ignore it.
+            cloudpickle.register_pickle_by_value(
+                inspect.getmodule(inspect.stack()[1][0])
+            )
+        except ValueError:
+            pass
+        self._send_raw(input_dict={"f": function, "l": lst})
+        return self._receive()
+
+    def _pool_open(self):
         path = os.path.abspath(os.path.join(__file__, "..", "__main__.py"))
         self._context = zmq.Context()
         self._socket = self._context.socket(zmq.PAIR)
@@ -82,9 +117,8 @@ class Pool(object):
             stderr=None,
             stdin=subprocess.PIPE,
         )
-        return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def _pool_close(self):
         self._send_raw(input_dict={"c": "close"})
         self._process.terminate()
         self._process.stdout.close()
@@ -95,34 +129,6 @@ class Pool(object):
         self._process = None
         self._socket = None
         self._context = None
-
-    def map(self, function, lst):
-        """
-        Map a given function on a list of attributes.
-
-        Args:
-            function: function to be applied to each element of the following list
-            lst (list): list of arguments the function should be applied on
-
-        Returns:
-            list: list of output generated from applying the function on the list of arguments
-        """
-        # Cloudpickle can either pickle by value or pickle by reference. The functions which are communicated have to
-        # be pickled by value rather than by reference, so the module which calls the map function is pickled by value.
-        # https://github.com/cloudpipe/cloudpickle#overriding-pickles-serialization-mechanism-for-importable-constructs
-        # inspect can help to find the module which is calling pympipool
-        # https://docs.python.org/3/library/inspect.html
-        # to learn more about inspect another good read is:
-        # http://pymotw.com/2/inspect/index.html#module-inspect
-        # 1 refers to 1 level higher than the map function
-        try:  # When executed in a jupyter notebook this can cause a ValueError - in this case we just ignore it.
-            cloudpickle.register_pickle_by_value(
-                inspect.getmodule(inspect.stack()[1][0])
-            )
-        except ValueError:
-            pass
-        self._send_raw(input_dict={"f": function, "l": lst})
-        return self._receive()
 
     def _send_raw(self, input_dict):
         self._socket.send(cloudpickle.dumps(input_dict))
