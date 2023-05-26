a6e577bd26d6ef77718919d226c42b7b797ea684
Jan Janssen <janssen@lanl.gov>
2023-05-25 22:14:04 -0600

    Add parallel test

--- tests/test_future.py
+++ tests/test_future.py
@@ -9,6 +9,19 @@ def calc(i):
     return np.array(i ** 2)
 
 
+def get_ranks(input_parameter, comm=None):
+    from mpi4py import MPI
+    size = MPI.COMM_WORLD.Get_size()
+    rank = MPI.COMM_WORLD.Get_rank()
+    if comm is not None:
+        size_new = comm.Get_size()
+        rank_new = comm.Get_rank()
+    else:
+        size_new = 0
+        rank_new = 0
+    return size, rank, size_new, rank_new, input_parameter
+
+
 class TestPool(unittest.TestCase):
     def test_pool_serial(self):
         with Pool(cores=1) as p:
@@ -19,3 +32,13 @@ class TestPool(unittest.TestCase):
         sleep(1)
         self.assertTrue(output.done())
         self.assertEqual(output.result(), 4)
+
+    def test_pool_parallel(self):
+        with Pool(cores=2, cores_per_task=2) as p:
+            output = p.submit(get_ranks, i=2)
+            self.assertTrue(isinstance(output, Future))
+            self.assertFalse(output.done())
+            p.update()
+        sleep(1)
+        self.assertTrue(output.done())
+        self.assertEqual(output.result(), [2, 0, 2, 0, 2])
