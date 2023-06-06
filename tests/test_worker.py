405649339fb1e5106cf4b5c3102067235f5222c9
Jan Janssen <janssen@lanl.gov>
2023-06-06 15:36:08 -0600

    Fix return values

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -28,7 +28,7 @@ class TestFuturePool(unittest.TestCase):
             sleep(1)
             self.assertTrue(output.done())
             self.assertEqual(len(p), 0)
-        self.assertEqual(output.result(), np.array(4))
+        self.assertEqual(output.result(), 4)
 
     def test_pool_serial_multi_core(self):
         with Worker(cores=2) as p:
@@ -53,4 +53,4 @@ class TestFuturePool(unittest.TestCase):
             oversubscribe=False,
             enable_flux_backend=False
         )
-        self.assertEqual(f.result(), np.array(4))
+        self.assertEqual(f.result(), 4)
