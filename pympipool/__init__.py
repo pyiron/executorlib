122b7b08e155a840fd7c76c795c68aa8d5eeaabd
Jan Janssen <janssen@lanl.gov>
2023-06-09 12:53:40 -0600

    Fix import

--- pympipool/__init__.py
+++ pympipool/__init__.py
@@ -1,3 +1,3 @@
 from pympipool.share.pool import Pool
-from pympipool.share.executor import Executor
+from pympipool.share.executor import Worker
 from pympipool.share.communication import SocketInterface
