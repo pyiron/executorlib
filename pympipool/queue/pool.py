2d631f0d16a5105ffbbe3694b5061aa22ae62be8
Jan Janssen <janssen@lanl.gov>
2023-07-12 16:37:15 -0600

    Integration with pysqa

---
+++ pympipool/queue/pool.py
@@ -0,0 +1,115 @@
+from pympipool.share.pool import Pool as PoolBase, MPISpawnPool as MPISpawnPoolBase
+from pympipool.share.serial import get_parallel_subprocess_command
+
+
+class Pool(PoolBase):
+    """
+    The pympipool.Pool behaves like the multiprocessing.Pool but it uses mpi4py to distribute tasks. In contrast to the
+    mpi4py.futures.MPIPoolExecutor the pympipool.Pool can be executed in a serial python process and does not require
+    the python script to be executed with MPI. Still internally the pympipool.Pool uses the
+    mpi4py.futures.MPIPoolExecutor, consequently it is primarily an abstraction of its functionality to improve the
+    usability in particular when used in combination with Jupyter notebooks.
+
+    Args:
+        max_workers (int): defines the total number of MPI ranks to use
+        cores_per_task (int): defines the number of MPI ranks per task
+        oversubscribe (bool): adds the `--oversubscribe` command line flag (OpenMPI only)
+
+    Simple example:
+        ```
+        import numpy as np
+        from pympipool import Pool
+
+        def calc(i):
+            return np.array(i ** 2)
+
+        with Pool(cores=2) as p:
+            print(p.map(func=calc, iterable=[1, 2, 3, 4]))
+        ```
+    """
+    def __init__(
+        self,
+        max_workers=1,
+        oversubscribe=False,
+        enable_flux_backend=False,
+        cwd=None,
+        queue_adapter=None,
+        queue_adapter_kwargs=None,
+    ):
+        super(PoolBase, self).__init__()
+        command_lst = get_parallel_subprocess_command(
+            port_selected=self._interface.bind_to_random_port(),
+            cores=max_workers,
+            cores_per_task=1,
+            oversubscribe=oversubscribe,
+            enable_flux_backend=enable_flux_backend,
+            enable_mpi4py_backend=True,
+        )
+        if queue_adapter is not None:
+            queue_adapter.submit(
+                working_directory=cwd,
+                cores=max_workers,
+                command=' '.join(command_lst),
+                **queue_adapter_kwargs
+            )
+        else:
+            self._interface.bootup(
+                command_lst=command_lst,
+                cwd=cwd,
+            )
+
+
+class MPISpawnPool(MPISpawnPoolBase):
+    """
+    The pympipool.MPISpawnPool behaves like the multiprocessing.Pool but it uses mpi4py to distribute tasks. In contrast
+    to the mpi4py.futures.MPIPoolExecutor the pympipool.MPISpawnPool can be executed in a serial python process and does
+    not require the python script to be executed with MPI. Still internally the pympipool.Pool uses the
+    mpi4py.futures.MPIPoolExecutor, consequently it is primarily an abstraction of its functionality to improve the
+    usability in particular when used in combination with Jupyter notebooks.
+
+    Args:
+        max_ranks (int): defines the total number of MPI ranks to use
+        ranks_per_task (int): defines the number of MPI ranks per task
+        oversubscribe (bool): adds the `--oversubscribe` command line flag (OpenMPI only)
+
+    Simple example:
+        ```
+        from pympipool import MPISpawnPool
+
+        def calc(i, comm):
+            return i, comm.Get_size(), comm.Get_rank()
+
+        with MPISpawnPool(max_ranks=4, ranks_per_task=2) as p:
+            print(p.map(func=calc, iterable=[1, 2, 3, 4]))
+        ```
+    """
+    def __init__(
+        self,
+        max_ranks=1,
+        ranks_per_task=1,
+        oversubscribe=False,
+        cwd=None,
+        queue_adapter=None,
+        queue_adapter_kwargs=None,
+    ):
+        super(MPISpawnPoolBase, self).__init__()
+        command_lst = get_parallel_subprocess_command(
+            port_selected=self._interface.bind_to_random_port(),
+            cores=max_ranks,
+            cores_per_task=ranks_per_task,
+            oversubscribe=oversubscribe,
+            enable_flux_backend=False,
+            enable_mpi4py_backend=True,
+        )
+        if queue_adapter is not None:
+            queue_adapter.submit(
+                working_directory=cwd,
+                cores=max_ranks,
+                command=' '.join(command_lst),
+                **queue_adapter_kwargs
+            )
+        else:
+            self._interface.bootup(
+                command_lst=command_lst,
+                cwd=cwd,
+            )
