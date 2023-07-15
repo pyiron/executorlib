e2e1005ef0fc4db7927bc99cf617c4515fd15a9b
Jan Janßen <janssen@mpie.de>
2023-07-15 11:46:40 -0600

    black formatting

--- pympipool/shared_functions/thread.py
+++ pympipool/shared_functions/thread.py
@@ -5,8 +5,18 @@ class RaisingThread(Thread):
     """
     Based on https://stackoverflow.com/questions/2829329/catch-a-threads-exception-in-the-caller-thread
     """
-    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, *, daemon=None):
-        super().__init__(group=group, target=target, name=name, args=args, kwargs=kwargs, daemon=daemon)
+
+    def __init__(
+        self, group=None, target=None, name=None, args=(), kwargs=None, *, daemon=None
+    ):
+        super().__init__(
+            group=group,
+            target=target,
+            name=name,
+            args=args,
+            kwargs=kwargs,
+            daemon=daemon,
+        )
         self._exception = None
 
     def run(self):
