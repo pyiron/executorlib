f0c694e0db94e256f04bfe9abbd73e9d536e0787
Jan Janßen <janssen@mpie.de>
2023-07-29 22:49:11 -0600

    More tests

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,6 +1,13 @@
 import os
 import unittest
-from pympipool.shared.connections import BaseInterface
+from pympipool.shared.connections import (
+    BaseInterface,
+    MpiExecInterface,
+    SlurmSubprocessInterface,
+    PysqaInterface,
+    FluxCmdInterface,
+    get_connection_interface
+)
 
 
 class Interface(BaseInterface):
@@ -33,3 +40,61 @@ class TestExecutor(unittest.TestCase):
     def test_poll(self):
         with self.assertRaises(NotImplementedError):
             self.interface.poll()
+
+
+class TestInterfaceConnection(unittest.TestCase):
+    def test_mpiexec(self):
+        interface = get_connection_interface(
+            cwd=os.path.abspath("."),
+            cores=1,
+            gpus_per_core=0,
+            oversubscribe=False,
+            enable_flux_backend=False,
+            enable_slurm_backend=False,
+            queue_adapter=None,
+            queue_type=None,
+            queue_adapter_kwargs=None,
+        )
+        self.assertIsInstance(interface, MpiExecInterface)
+
+    def test_slurm(self):
+        interface = get_connection_interface(
+            cwd=os.path.abspath("."),
+            cores=1,
+            gpus_per_core=0,
+            oversubscribe=False,
+            enable_flux_backend=False,
+            enable_slurm_backend=True,
+            queue_adapter=None,
+            queue_type=None,
+            queue_adapter_kwargs=None,
+        )
+        self.assertIsInstance(interface, SlurmSubprocessInterface)
+
+    def test_pysqa(self):
+        interface = get_connection_interface(
+            cwd=os.path.abspath("."),
+            cores=1,
+            gpus_per_core=0,
+            oversubscribe=False,
+            enable_flux_backend=False,
+            enable_slurm_backend=False,
+            queue_adapter=True,
+            queue_type=None,
+            queue_adapter_kwargs=None,
+        )
+        self.assertIsInstance(interface, PysqaInterface)
+
+    def test_flux_cmd(self):
+        interface = get_connection_interface(
+            cwd=os.path.abspath("."),
+            cores=1,
+            gpus_per_core=0,
+            oversubscribe=False,
+            enable_flux_backend=True,
+            enable_slurm_backend=False,
+            queue_adapter=None,
+            queue_type=None,
+            queue_adapter_kwargs=None,
+        )
+        self.assertIsInstance(interface, FluxCmdInterface)
