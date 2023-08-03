f4ae88bdcce19e1aafbb573b4f7a40f1c645fda6
Jan Janssen <janssen@lanl.gov>
2023-08-03 15:15:28 -0600

    Add second test call main as function

--- tests/test_serial.py
+++ tests/test_serial.py
@@ -9,8 +9,13 @@ def calc(i):
     return i
 
 
+def submit_from_thread(socket):
+    socket.send(cloudpickle.dumps({"fn": calc, 'args': (), "kwargs": {"i": 2}}))
+    socket.send(cloudpickle.dumps({"shutdown": True, "wait": True}))
+
+
 class TestSerial(unittest.TestCase):
-    def test_main(self):
+    def test_main_as_thread(self):
         context = zmq.Context()
         socket = context.socket(zmq.PAIR)
         port = socket.bind_to_random_port("tcp://*")
@@ -19,6 +24,18 @@ class TestSerial(unittest.TestCase):
         socket.send(cloudpickle.dumps({"fn": calc, 'args': (), "kwargs": {"i": 2}}))
         self.assertEqual(cloudpickle.loads(socket.recv()), {'result': 2})
         socket.send(cloudpickle.dumps({"shutdown": True, "wait": True}))
+        self.assertEqual(cloudpickle.loads(socket.recv()), {'result': True})
+        socket.close()
+        context.term()
+
+    def test_submit_as_thread(self):
+        context = zmq.Context()
+        socket = context.socket(zmq.PAIR)
+        port = socket.bind_to_random_port("tcp://*")
+        t = Thread(target=submit_from_thread, kwargs={"socket": socket})
+        t.start()
+        main(argument_lst=["--zmqport", str(port)])
+        self.assertEqual(cloudpickle.loads(socket.recv()), {'result': 2})
         self.assertEqual(cloudpickle.loads(socket.recv()), {'result': True})
         socket.close()
         context.term()
