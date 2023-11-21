a704d20a8b1d85fb8461b4c84ae9f768c904bb1c
Jan Janssen <janssen@mpie.de>
2023-11-21 11:05:00 +0100

    Add tests for internal functionality

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,17 +1,30 @@
+from concurrent.futures import Future
+import queue
+
 from unittest import TestCase
 
-from pympipool.shell.executor import ShellStaticExecutor, ShellExecutor
+from pympipool.shell.executor import ShellStaticExecutor, ShellExecutor, execute_single_task
 
 
-class StaticExecutorTest(TestCase):
-    def test_shell_static(self):
+class ShellExecutorTest(TestCase):
+    def test_execute_single_task(self):
+        test_queue = queue.Queue()
+        f = Future()
+        test_queue.put({"future": f, "args": [["echo", "test"]], "kwargs": {"universal_newlines": True}})
+        test_queue.put({"shutdown": True})
+        self.assertFalse(f.done())
+        execute_single_task(future_queue=test_queue)
+        self.assertTrue(f.done())
+        self.assertEqual("test\n", f.result())
+
+    def test_shell_static_executor(self):
         with ShellStaticExecutor() as exe:
             future = exe.submit(["echo", "test"], universal_newlines=True)
             self.assertFalse(future.done())
             self.assertEqual("test\n", future.result())
             self.assertTrue(future.done())
 
-    def test_shell(self):
+    def test_shell_executor(self):
         with ShellExecutor(max_workers=2) as exe:
             f_1 = exe.submit(["echo", "test_1"], universal_newlines=True)
             f_2 = exe.submit(["echo", "test_2"], universal_newlines=True)
