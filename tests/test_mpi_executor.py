7d9ca11a81cad4dc4ad1845dcbf021ff416bd050
Jan Janssen <janssen@mpie.de>
2024-04-18 15:33:21 -0500

    Merge tests

--- tests/test_mpi_executor.py
+++ tests/test_mpi_executor.py
@@ -8,6 +8,10 @@ def calc(i):
     return i
 
 
+def echo_funct(i):
+    return i
+
+
 def mpi_funct(i):
     from mpi4py import MPI
 
@@ -84,3 +88,33 @@ class TestExecutorBackend(unittest.TestCase):
             Executor(max_cores=1, cores_per_worker=1, threads_per_core=2, hostname_localhost=True, backend="mpi")
         with self.assertRaises(TypeError):
             Executor(max_cores=1, cores_per_worker=1, gpus_per_worker=1, hostname_localhost=True, backend="mpi")
+
+
+class TestTask(unittest.TestCase):
+    def test_echo(self):
+        with PyMPIExecutor(max_workers=1, cores_per_worker=2, hostname_localhost=True) as p:
+            cloudpickle_register(ind=1)
+            output = p.submit(echo_funct, 2).result()
+        self.assertEqual(output, [2, 2])
+
+    def test_mpi(self):
+        with PyMPIExecutor(max_workers=1, cores_per_worker=2, hostname_localhost=True) as p:
+            cloudpickle_register(ind=1)
+            output = p.submit(mpi_funct, 2).result()
+        self.assertEqual(output, [(2, 2, 0), (2, 2, 1)])
+
+    def test_mpi_multiple(self):
+        with PyMPIExecutor(max_workers=1, cores_per_worker=2, hostname_localhost=True) as p:
+            cloudpickle_register(ind=1)
+            fs1 = p.submit(mpi_funct, 1)
+            fs2 = p.submit(mpi_funct, 2)
+            fs3 = p.submit(mpi_funct, 3)
+            output = [
+                fs1.result(),
+                fs2.result(),
+                fs3.result(),
+            ]
+        self.assertEqual(
+            output,
+            [[(1, 2, 0), (1, 2, 1)], [(2, 2, 0), (2, 2, 1)], [(3, 2, 0), (3, 2, 1)]],
+        )
