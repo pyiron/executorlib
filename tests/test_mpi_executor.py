aaadf69052441b8b69996324cf45e44cc603d585
Jan Janssen <janssen@mpie.de>
2024-04-18 15:09:13 -0500

    block_allocation=True

--- tests/test_mpi_executor.py
+++ tests/test_mpi_executor.py
@@ -53,7 +53,7 @@ class TestMetaExecutor(unittest.TestCase):
 
 class TestExecutorBackend(unittest.TestCase):
     def test_meta_executor_serial(self):
-        with Executor(max_workers=2, hostname_localhost=True, backend="mpi") as exe:
+        with Executor(max_cores=2, hostname_localhost=True, backend="mpi", block_allocation=True) as exe:
             cloudpickle_register(ind=1)
             fs_1 = exe.submit(calc, 1)
             fs_2 = exe.submit(calc, 2)
@@ -63,7 +63,7 @@ class TestExecutorBackend(unittest.TestCase):
             self.assertTrue(fs_2.done())
 
     def test_meta_executor_single(self):
-        with Executor(max_workers=1, hostname_localhost=True, backend="mpi") as exe:
+        with Executor(max_cores=1, hostname_localhost=True, backend="mpi", block_allocation=True) as exe:
             cloudpickle_register(ind=1)
             fs_1 = exe.submit(calc, 1)
             fs_2 = exe.submit(calc, 2)
@@ -73,7 +73,7 @@ class TestExecutorBackend(unittest.TestCase):
             self.assertTrue(fs_2.done())
 
     def test_meta_executor_parallel(self):
-        with Executor(max_workers=1, cores_per_worker=2, hostname_localhost=True, backend="mpi") as exe:
+        with Executor(max_cores=2, cores_per_worker=2, hostname_localhost=True, backend="mpi", block_allocation=True) as exe:
             cloudpickle_register(ind=1)
             fs_1 = exe.submit(mpi_funct, 1)
             self.assertEqual(fs_1.result(), [(1, 2, 0), (1, 2, 1)])
@@ -81,6 +81,6 @@ class TestExecutorBackend(unittest.TestCase):
 
     def test_errors(self):
         with self.assertRaises(TypeError):
-            Executor(max_workers=1, cores_per_worker=1, threads_per_core=2, hostname_localhost=True, backend="mpi")
+            Executor(max_cores=1, cores_per_worker=1, threads_per_core=2, hostname_localhost=True, backend="mpi")
         with self.assertRaises(TypeError):
-            Executor(max_workers=1, cores_per_worker=1, gpus_per_worker=1, hostname_localhost=True, backend="mpi")
+            Executor(max_cores=1, cores_per_worker=1, gpus_per_worker=1, hostname_localhost=True, backend="mpi")
