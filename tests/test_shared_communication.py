8a61280b2d1d77dc34273959a90bba987b9bec2b
Jan Janssen <janssen@mpie.de>
2024-04-18 14:12:47 -0500

    Restructure tests

--- tests/test_shared_communication.py
+++ tests/test_shared_communication.py
@@ -3,7 +3,15 @@ import sys
 
 import numpy as np
 import unittest
-from pympipool.shared.communication import SocketInterface
+import zmq
+
+from pympipool.shared.communication import (
+    interface_connect,
+    interface_shutdown,
+    interface_send,
+    interface_receive,
+    SocketInterface,
+)
 from pympipool.shared.executorbase import cloudpickle_register
 from pympipool.shared.interface import MpiExecInterface
 
@@ -37,3 +45,18 @@ class TestInterface(unittest.TestCase):
             interface.send_and_receive_dict(input_dict=task_dict), np.array(4)
         )
         interface.shutdown(wait=True)
+
+
+class TestZMQ(unittest.TestCase):
+    def test_initialize_zmq(self):
+        message = "test"
+        host = "localhost"
+
+        context_server = zmq.Context()
+        socket_server = context_server.socket(zmq.PAIR)
+        port = str(socket_server.bind_to_random_port("tcp://*"))
+        context_client, socket_client = interface_connect(host=host, port=port)
+        interface_send(socket=socket_server, result_dict={"message": message})
+        self.assertEqual(interface_receive(socket=socket_client), {"message": message})
+        interface_shutdown(socket=socket_client, context=context_client)
+        interface_shutdown(socket=socket_server, context=context_server)
