6e15d66ce231add88f0c152d33338a177dece76d
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-04-21 18:48:17 -0500

    Update test_results.py

--- tests/benchmark/test_results.py
+++ tests/benchmark/test_results.py
@@ -12,7 +12,7 @@ class TestResults(unittest.TestCase):
         self.assertTrue(timing_dict["block_allocation"] < timing_dict["process"] * 1.1) 
         self.assertTrue(timing_dict["pympipool"] < timing_dict["process"] * 1.35)
         self.assertTrue(timing_dict["process"] < timing_dict["mpi4py"])
-        self.assertTrue(timing_dict["pympipool"] < timing_dict["mpi4py"])
+        self.assertTrue(timing_dict["block_allocation"] < timing_dict["mpi4py"])
         self.assertTrue(timing_dict["mpi4py"] < timing_dict["process"] * 1.15)
         self.assertTrue(timing_dict["thread"] < timing_dict["static"])
         self.assertTrue(timing_dict["mpi4py"] < timing_dict["thread"])
