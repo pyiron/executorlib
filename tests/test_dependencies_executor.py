fc2f7060988d483d88724bd0a3e26fab5fb0820e
Jan Janssen <janssen@mpie.de>
2024-04-21 15:54:42 -0500

    Fix MacOs tests

--- tests/test_dependencies_executor.py
+++ tests/test_dependencies_executor.py
@@ -12,7 +12,7 @@ def add_function(parameter_1, parameter_2):
 
 class TestExecutorWithDependencies(unittest.TestCase):
     def test_executor(self):
-        with ExecutorWithDependencies(max_cores=1, backend="mpi") as exe:
+        with ExecutorWithDependencies(max_cores=1, backend="mpi", hostname_localhost=True) as exe:
             cloudpickle_register(ind=1)
             future_1 = exe.submit(add_function, 1, parameter_2=2)
             future_2 = exe.submit(add_function, 1, parameter_2=future_1)
