a25a3d0dd11b2b4f8cd5223cc961f6ff91933ab5
Jan Janssen <janssen@mpie.de>
2024-04-21 15:37:29 -0500

    use executor shutdown

--- pympipool/dependencies/executor.py
+++ pympipool/dependencies/executor.py
@@ -3,12 +3,12 @@ from queue import Queue, Empty
 from time import sleep
 
 from pympipool.scheduler import create_executor
-from pympipool.shared.executorbase import ExecutorSteps
+from pympipool.shared.executorbase import ExecutorSteps, ExecutorBase
 from pympipool.shared.thread import RaisingThread
 
 
 def run_task_with_dependencies(
-    future_queue: Queue, executor_queue: Queue, sleep_interval: float = 0.1
+    future_queue: Queue, executor: ExecutorBase, sleep_interval: float = 0.1
 ):
     wait_lst = []
     while True:
@@ -21,7 +21,7 @@ def run_task_with_dependencies(
             and "shutdown" in task_dict.keys()
             and task_dict["shutdown"]
         ):
-            executor_queue.put(task_dict)
+            executor.shutdown(wait=task_dict["wait"])
             future_queue.task_done()
             future_queue.join()
             break
@@ -43,7 +43,7 @@ def run_task_with_dependencies(
                     key: value if not isinstance(value, Future) else value.result()
                     for key, value in task_dict["kwargs"].items()
                 }
-                executor_queue.put(task_dict)
+                executor._future_queue.put(task_dict)
             else:
                 task_dict["future_lst"] = future_lst
                 wait_lst.append(task_dict)
@@ -53,7 +53,7 @@ def run_task_with_dependencies(
             for task_wait_dict in wait_lst:
                 if all([future.done() for future in task_dict["future_lst"]]):
                     del task_dict["future_lst"]
-                    executor_queue.put(task_dict)
+                    executor._future_queue.put(task_dict)
                 else:
                     wait_tmp_lst.append(task_wait_dict)
             wait_lst = wait_tmp_lst
@@ -64,14 +64,13 @@ def run_task_with_dependencies(
 class ExecutorWithDependencies(ExecutorSteps):
     def __init__(self, *args, sleep_interval: float = 0.1, **kwargs):
         super().__init__()
-        self._executor_internal = create_executor(*args, **kwargs)
         self._set_process(
             RaisingThread(
                 target=run_task_with_dependencies,
                 kwargs={
                     # Executor Arguments
                     "future_queue": self._future_queue,
-                    "executor_queue": self._executor_internal._future_queue,
+                    "executor_queue": create_executor(*args, **kwargs),
                     "sleep_interval": sleep_interval,
                 },
             )
