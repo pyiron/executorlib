8da17494d90cf864efbbbfce64c992dea39752bd
Jan Janssen <janssen@mpie.de>
2024-04-21 15:08:49 -0500

    fix bug

--- tests/test_dependencies_executor.py
+++ tests/test_dependencies_executor.py
@@ -2,6 +2,7 @@ import unittest
 from time import sleep
 
 from pympipool.dependencies import ExecutorWithDependencies
+from pympipool.shared.executorbase import cloudpickle_register
 
 
 def add_function(parameter_1, parameter_2):
@@ -12,6 +13,7 @@ def add_function(parameter_1, parameter_2):
 class TestExecutorWithDependencies(unittest.TestCase):
     def test_executor(self):
         with ExecutorWithDependencies(max_cores=1, backend="mpi") as exe:
+            cloudpickle_register(ind=1)
             future_1 = exe.submit(add_function, 1, parameter_2=2)
             future_2 = exe.submit(add_function, 1, parameter_2=future_1)
             self.assertEqual(future_2.result(), 4)
