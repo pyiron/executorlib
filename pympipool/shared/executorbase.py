f9b65ce398b2d17a62129102c4491860150f87b9
Jan Janssen <janssen@mpie.de>
2024-04-22 09:10:31 -0500

    Store functions in central place

--- pympipool/shared/executorbase.py
+++ pympipool/shared/executorbase.py
@@ -13,6 +13,10 @@ import cloudpickle
 from pympipool.shared.communication import interface_bootup
 from pympipool.shared.thread import RaisingThread
 from pympipool.shared.interface import BaseInterface
+from pympipool.shared.inputcheck import (
+    check_resource_dict,
+    check_resource_dict_is_empty,
+)
 
 
 class ExecutorBase(FutureExecutor):
@@ -66,14 +70,8 @@ class ExecutorBase(FutureExecutor):
         Returns:
             A Future representing the given call.
         """
-        if len(resource_dict) > 0:
-            raise ValueError(
-                "When block_allocation is enabled, the resource requirements have to be defined on the executor level."
-            )
-        if "resource_dict" in inspect.signature(fn).parameters.keys():
-            raise ValueError(
-                "The parameter resource_dict is used internally in pympipool, so it cannot be used as parameter in the submitted functions."
-            )
+        check_resource_dict_is_empty(resource_dict=resource_dict)
+        check_resource_dict(function=fn)
         f = Future()
         self._future_queue.put({"fn": fn, "args": args, "kwargs": kwargs, "future": f})
         return f
@@ -175,10 +173,7 @@ class ExecutorSteps(ExecutorBase):
         Returns:
             A Future representing the given call.
         """
-        if "resource_dict" in inspect.signature(fn).parameters.keys():
-            raise ValueError(
-                "The parameter resource_dict is used internally in pympipool, so it cannot be used as parameter in the submitted functions."
-            )
+        check_resource_dict(function=fn)
         f = Future()
         self._future_queue.put(
             {
--- pympipool/shared/inputcheck.py
+++ pympipool/shared/inputcheck.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 def check_oversubscribe(oversubscribe):
     if oversubscribe:
         raise ValueError(
@@ -37,4 +40,19 @@ def check_executor(executor):
     if executor is not None:
         raise ValueError(
             "The executor parameter is only supported for the flux framework backend."
+        )
+
+
+def check_resource_dict(function):
+    if "resource_dict" in inspect.signature(function).parameters.keys():
+        raise ValueError(
+            "The parameter resource_dict is used internally in pympipool, "
+            "so it cannot be used as parameter in the submitted functions."
+        )
+
+
+def check_resource_dict_is_empty(resource_dict):
+    if len(resource_dict) > 0:
+        raise ValueError(
+            "When block_allocation is enabled, the resource requirements have to be defined on the executor level."
         )
