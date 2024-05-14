a0003ddd911a52f04c1e6b51f747f097832c835f
Jan Janssen <janssen@mpie.de>
2024-05-14 12:58:52 -0500

    fixes

--- pympipool/shared/__init__.py
+++ pympipool/shared/__init__.py
@@ -9,3 +9,17 @@ from pympipool.shared.communication import (
 from pympipool.shared.executorbase import cancel_items_in_queue
 from pympipool.shared.thread import RaisingThread
 from pympipool.shared.interface import MpiExecInterface, SrunInterface
+
+
+__all__ = [
+    SocketInterface,
+    interface_bootup,
+    interface_connect,
+    interface_send,
+    interface_shutdown,
+    interface_receive,
+    cancel_items_in_queue,
+    RaisingThread,
+    MpiExecInterface,
+    SrunInterface,
+]
