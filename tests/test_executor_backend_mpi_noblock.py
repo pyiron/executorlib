b41acbaad67807ac7e2b929093b460843207ab6a
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-10-29 11:47:59 +0100

    Add additional tests to improve coverage (#464)

--- tests/test_executor_backend_mpi_noblock.py
+++ tests/test_executor_backend_mpi_noblock.py
@@ -13,11 +13,27 @@ def resource_dict(resource_dict):
 
 
 class TestExecutorBackend(unittest.TestCase):
-    def test_meta_executor_serial(self):
+    def test_meta_executor_serial_with_dependencies(self):
         with Executor(
             max_cores=2,
             backend="local",
             block_allocation=False,
+            disable_dependencies=True,
+        ) as exe:
+            cloudpickle_register(ind=1)
+            fs_1 = exe.submit(calc, 1)
+            fs_2 = exe.submit(calc, 2)
+            self.assertEqual(fs_1.result(), 1)
+            self.assertEqual(fs_2.result(), 2)
+            self.assertTrue(fs_1.done())
+            self.assertTrue(fs_2.done())
+
+    def test_meta_executor_serial_without_dependencies(self):
+        with Executor(
+            max_cores=2,
+            backend="local",
+            block_allocation=False,
+            disable_dependencies=False,
         ) as exe:
             cloudpickle_register(ind=1)
             fs_1 = exe.submit(calc, 1)
