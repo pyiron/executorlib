c65100b29f23a47d0e9b2b1a9fd909b2693d7f3d
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-10-29 16:32:07 +0100

    Add pysqa command which raises an exception (#465)
    * Add pysqa command which raises an exception
    
    * fix type hints
    
    * fix tests
    
    * another test
    
    * more fixes
    
    * add shell option

---
+++ tests/test_pysqa_subprocess.py
@@ -0,0 +1,45 @@
+import unittest
+
+try:
+    from executorlib.standalone.cache.queue import _pysqa_execute_command
+
+    skip_pysqa_test = False
+except ImportError:
+    skip_pysqa_test = True
+
+
+@unittest.skipIf(
+    skip_pysqa_test, "pysqa is not installed, so the pysqa tests are skipped."
+)
+class TestPysqaExecuteCommand(unittest.TestCase):
+    def test_pysqa_execute_command_list(self):
+        out = _pysqa_execute_command(
+            commands=["echo", "test"],
+            working_directory=None,
+            split_output=True,
+            shell=True,
+            error_filename="pysqa.err",
+        )
+        self.assertEqual(len(out), 2)
+        self.assertEqual("test", out[0])
+
+    def test_pysqa_execute_command_string(self):
+        out = _pysqa_execute_command(
+            commands="echo test",
+            working_directory=None,
+            split_output=False,
+            shell=False,
+            error_filename="pysqa.err",
+        )
+        self.assertEqual(len(out), 5)
+        self.assertEqual("test\n", out)
+
+    def test_pysqa_execute_command_fail(self):
+        with self.assertRaises(FileNotFoundError):
+            _pysqa_execute_command(
+                commands=["no/executable/available"],
+                working_directory=None,
+                split_output=True,
+                shell=False,
+                error_filename="pysqa.err",
+            )
