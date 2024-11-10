cdba297e31c552c71f8d5ab7a517ee6ae77d7108
Jan Janssen <janssen@mpie.de>
2024-11-10 17:26:09 +0100

    Add tests

--- tests/test_executor_backend_mpi.py
+++ tests/test_executor_backend_mpi.py
@@ -14,6 +14,11 @@ def calc(i):
     return i
 
 
+def calc_sleep(i):
+    time.sleep(i)
+    return i
+
+
 def mpi_funct(i):
     from mpi4py import MPI
 
@@ -91,6 +96,32 @@ class TestExecutorBackend(unittest.TestCase):
 class TestExecutorBackendCache(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree("./cache")
+
+    def test_executor_cache_bypass(self):
+        with Executor(max_workers=2, backend="local", block_allocation=True, cache_directory="./cache") as exe:
+            cloudpickle_register(ind=1)
+            time_1 = time.time()
+            fs_1 = exe.submit(calc_sleep, 1)
+            fs_2 = exe.submit(calc_sleep, 1)
+            self.assertEqual(fs_1.result(), 1)
+            self.assertTrue(fs_1.done())
+            time_2 = time.time()
+            self.assertEqual(fs_2.result(), 1)
+            self.assertTrue(fs_2.done())
+            time_3 = time.time()
+            self.assertTrue(time_2 - time_1 > 1)
+            self.assertTrue(time_3 - time_1 > 1)
+            time_4 = time.time()
+            fs_3 = exe.submit(calc_sleep, 1)
+            fs_4 = exe.submit(calc_sleep, 1)
+            self.assertEqual(fs_3.result(), 1)
+            self.assertTrue(fs_3.done())
+            time_5 = time.time()
+            self.assertEqual(fs_4.result(), 1)
+            self.assertTrue(fs_4.done())
+            time_6 = time.time()
+            self.assertTrue(time_5 - time_4 < 1)
+            self.assertTrue(time_6 - time_4 < 1)
 
     @unittest.skipIf(
         skip_mpi4py_test, "mpi4py is not installed, so the mpi4py tests are skipped."
