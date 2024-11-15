c1b8b227744db3c690e18c48182190f45812e44c
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-11-15 20:19:36 +0100

    Queuing system submission: check if the job is already waiting in the queue or currently running. (#499)
    * Queuing system submission: check if the job is already waiting in the queue or currently running.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update shared.py
    
    * fix tests
    
    * Add tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * test without working directoru parameter
    
    * more fixes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * more tests
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- executorlib/cache/queue_spawner.py
+++ executorlib/cache/queue_spawner.py
@@ -1,24 +1,29 @@
 import os
 import subprocess
-from typing import List, Optional, Union
+from typing import List, Optional, Tuple, Union
 
 from pysqa import QueueAdapter
 
+from executorlib.standalone.hdf import dump, get_queue_id
+from executorlib.standalone.inputcheck import check_file_exists
+
 
 def execute_with_pysqa(
-    command: str,
-    resource_dict: dict,
-    task_dependent_lst: List[int] = [],
+    command: list,
+    task_dependent_lst: list[int] = [],
+    file_name: Optional[str] = None,
+    resource_dict: Optional[dict] = None,
     config_directory: Optional[str] = None,
     backend: Optional[str] = None,
     cache_directory: Optional[str] = None,
-) -> int:
+) -> Tuple[int, int]:
     """
     Execute a command by submitting it to the queuing system
 
     Args:
         command (list): The command to be executed.
         task_dependent_lst (list): A list of subprocesses that the current subprocess depends on. Defaults to [].
+        file_name (str): Name of the HDF5 file which contains the Python function
         resource_dict (dict): resource dictionary, which defines the resources used for the execution of the function.
                               Example resource dictionary: {
                                   cwd: None,
@@ -30,37 +35,42 @@ def execute_with_pysqa(
     Returns:
         int: queuing system ID
     """
-    if resource_dict is None:
-        resource_dict = {}
-    if "cwd" in resource_dict and resource_dict["cwd"] is not None:
-        cwd = resource_dict["cwd"]
-    else:
-        cwd = cache_directory
+    check_file_exists(file_name=file_name)
+    queue_id = get_queue_id(file_name=file_name)
     qa = QueueAdapter(
         directory=config_directory,
         queue_type=backend,
         execute_command=_pysqa_execute_command,
     )
-    submit_kwargs = {
-        "command": " ".join(command),
-        "dependency_list": [str(qid) for qid in task_dependent_lst],
-        "working_directory": os.path.abspath(cwd),
-    }
-    if "cwd" in resource_dict:
-        del resource_dict["cwd"]
-    unsupported_keys = [
-        "threads_per_core",
-        "gpus_per_core",
-        "openmpi_oversubscribe",
-        "slurm_cmd_args",
-    ]
-    for k in unsupported_keys:
-        if k in resource_dict:
-            del resource_dict[k]
-    if "job_name" not in resource_dict:
-        resource_dict["job_name"] = "pysqa"
-    submit_kwargs.update(resource_dict)
-    return qa.submit_job(**submit_kwargs)
+    if queue_id is None or qa.get_status_of_job(process_id=queue_id) is None:
+        if resource_dict is None:
+            resource_dict = {}
+        if "cwd" in resource_dict and resource_dict["cwd"] is not None:
+            cwd = resource_dict["cwd"]
+        else:
+            cwd = cache_directory
+        submit_kwargs = {
+            "command": " ".join(command),
+            "dependency_list": [str(qid) for qid in task_dependent_lst],
+            "working_directory": os.path.abspath(cwd),
+        }
+        if "cwd" in resource_dict:
+            del resource_dict["cwd"]
+        unsupported_keys = [
+            "threads_per_core",
+            "gpus_per_core",
+            "openmpi_oversubscribe",
+            "slurm_cmd_args",
+        ]
+        for k in unsupported_keys:
+            if k in resource_dict:
+                del resource_dict[k]
+        if "job_name" not in resource_dict:
+            resource_dict["job_name"] = "pysqa"
+        submit_kwargs.update(resource_dict)
+        queue_id = qa.submit_job(**submit_kwargs)
+        dump(file_name=file_name, data_dict={"queue_id": queue_id})
+    return queue_id
 
 
 def _pysqa_execute_command(
