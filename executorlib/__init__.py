0b38e5fd76bd9e0b06aacbba34982d5ea3f1904f
Jan Janßen <janssen@mpie.de>
2025-02-01 12:44:55 +0100

    move interfaces to individual modules

--- executorlib/__init__.py
+++ executorlib/__init__.py
@@ -1,11 +1,13 @@
 from executorlib._version import get_versions as _get_versions
-from executorlib.interfaces import (
+from executorlib.interfaces.flux import (
     FluxAllocationExecutor,
     FluxSubmissionExecutor,
-    LocalExecutor,
+)
+from executorlib.interfaces.local import LocalExecutor
+from executorlib.interfaces.slurm import (
     SlurmAllocationExecutor,
     SlurmSubmissionExecutor,
 )
 
 __version__ = _get_versions()["version"]
-__all__: list = []
+__all__: list = ["FluxAllocationExecutor", "FluxSubmissionExecutor", "LocalExecutor", "SlurmAllocationExecutor", "SlurmSubmissionExecutor"]
