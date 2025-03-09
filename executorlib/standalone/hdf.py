5e80e91af4f97b7fad588504cf9631d3d5724f06
Jan Janßen <janssen@mpie.de>
2025-03-09 17:58:33 +0100

    more fixes

--- executorlib/standalone/hdf.py
+++ executorlib/standalone/hdf.py
@@ -105,13 +105,15 @@ def get_queue_id(file_name: Optional[str]) -> Optional[int]:
 def get_cache_data(cache_directory: str) -> list[dict]:
     file_lst = []
     for task_key in os.listdir(cache_directory):
-        file_name = os.path.join(cache_directory, task_key, "cache.h5in")
-        with h5py.File(file_name, "r") as hdf:
-            file_content_dict = {
-                key: cloudpickle.loads(np.void(hdf["/" + key]))
-                for key in group_dict.values()
-                if key in hdf
-            }
-        file_content_dict["filename"] = file_name
-        file_lst.append(file_content_dict)
+        file_name = os.path.join(cache_directory, task_key, "cache.h5out")
+        os.makedirs(os.path.join(cache_directory, task_key), exist_ok=True)
+        if os.path.exists(file_name):
+            with h5py.File(file_name, "r") as hdf:
+                file_content_dict = {
+                    key: cloudpickle.loads(np.void(hdf["/" + key]))
+                    for key in group_dict.values()
+                    if key in hdf
+                }
+            file_content_dict["filename"] = file_name
+            file_lst.append(file_content_dict)
     return file_lst
