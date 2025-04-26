22eed9d06e4b79b427c3ab4dd03f242a351a4c13
Jan Janßen <janssen@mpie.de>
2025-04-26 09:14:04 +0200

    fix backends

--- executorlib/backend/cache_serial.py
+++ executorlib/backend/cache_serial.py
@@ -1,6 +1,6 @@
 import sys
 
-from executorlib.task_scheduler.file import backend_execute_task_in_file
+from executorlib.task_scheduler.file.backend import backend_execute_task_in_file
 
 if __name__ == "__main__":
     backend_execute_task_in_file(file_name=sys.argv[1])
