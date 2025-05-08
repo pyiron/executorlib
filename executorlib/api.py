bac1a92bf7322312783148300a78d9084e985f1c
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-05-08 09:35:44 +0200

    Adopt central API interface (#644)
    Following the [decision for unifying programming interfaces](https://github.com/pyiron/decisions/blob/main/decisions/25-001-split-api.md) within the pyiron project.

---
+++ executorlib/api.py
@@ -0,0 +1,33 @@
+"""
+External application programming interface (API) following the semantic versioning this inteface is promised to remain
+stable during minor releases and any change in the interface leads to a major version bump. External libraries should
+only use the functionality in this API in combination with the user interface defined in the root __init__.py, all other
+functionality is considered internal and might change during minor releases.
+"""
+
+from executorlib.standalone.command import get_command_path
+from executorlib.standalone.interactive.communication import (
+    SocketInterface,
+    interface_bootup,
+    interface_connect,
+    interface_receive,
+    interface_send,
+    interface_shutdown,
+)
+from executorlib.standalone.interactive.spawner import MpiExecSpawner, SubprocessSpawner
+from executorlib.standalone.queue import cancel_items_in_queue
+from executorlib.standalone.serialize import cloudpickle_register
+
+__all__: list[str] = [
+    "cancel_items_in_queue",
+    "cloudpickle_register",
+    "get_command_path",
+    "interface_bootup",
+    "interface_connect",
+    "interface_receive",
+    "interface_send",
+    "interface_shutdown",
+    "MpiExecSpawner",
+    "SocketInterface",
+    "SubprocessSpawner",
+]
--- executorlib/standalone/__init__.py
+++ executorlib/standalone/__init__.py
@@ -3,30 +3,3 @@ Submodules in the executorlib.standalone module do not depend on other modules o
 separation simplifies the development, testing and debugging. The functionality in executorlib.standalone is designed
 to be used independently in other libraries.
 """
-
-from executorlib.standalone.command import get_command_path
-from executorlib.standalone.interactive.communication import (
-    SocketInterface,
-    interface_bootup,
-    interface_connect,
-    interface_receive,
-    interface_send,
-    interface_shutdown,
-)
-from executorlib.standalone.interactive.spawner import MpiExecSpawner, SubprocessSpawner
-from executorlib.standalone.queue import cancel_items_in_queue
-from executorlib.standalone.serialize import cloudpickle_register
-
-__all__: list[str] = [
-    "cancel_items_in_queue",
-    "cloudpickle_register",
-    "get_command_path",
-    "interface_bootup",
-    "interface_connect",
-    "interface_receive",
-    "interface_send",
-    "interface_shutdown",
-    "MpiExecSpawner",
-    "SocketInterface",
-    "SubprocessSpawner",
-]
