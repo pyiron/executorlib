b9928fe1ac7291e2f0ccf211d1523377d5488f93
Jan Janssen <janssen@mpie.de>
2025-05-27 07:00:04 +0200

    Define ExecutorBase in base module
    This is helpful for identifying if a software package received an executor from executorlib.
    ```python
    from executorlib import SingleNodeExecutor
    from executorlib import BaseExecutor
    
    exe = SingleNodeExecutor()
    print(isinstance(exe, ExecutorBase))
    ```

--- executorlib/__init__.py
+++ executorlib/__init__.py
@@ -1,3 +1,4 @@
+from executorlib.executor.base import ExecutorBase
 from executorlib.executor.flux import (
     FluxClusterExecutor,
     FluxJobExecutor,
@@ -13,6 +14,7 @@ from . import _version
 
 __all__: list[str] = [
     "get_cache_data",
+    "ExecutorBase",
     "FluxJobExecutor",
     "FluxClusterExecutor",
     "SingleNodeExecutor",
