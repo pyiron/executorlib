f259b0a4fd7cd2c86d623fc307bc3e87559f2e8d
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-06-14 16:59:27 -0600

    [Documentation] Add docstrings for modules (#687)
    * [Documentation] Add docstrings for modules
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ executorlib/executor/__init__.py
@@ -0,0 +1,5 @@
+"""
+The executor module provides five different executor classes the user interacts with, namely: SingleNodeExecutor,
+SlurmClusterExecutor, FluxClusterExecutor, SlurmJobExecutor and FluxJobExecutor. In addition, it includes a BaseExecutor
+class to validate a given executor object is based on executorlib.
+"""
