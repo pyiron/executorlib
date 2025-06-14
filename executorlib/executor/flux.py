99cc72050c384d71e0687013e3804d2730dc4afa
Jan Janssen <janssen@mpie.de>
2025-06-13 21:11:06 -0600

    ignore # noqa

--- executorlib/executor/flux.py
+++ executorlib/executor/flux.py
@@ -337,7 +337,7 @@ class FluxClusterExecutor(BaseExecutor):
             from executorlib.task_scheduler.file.task_scheduler import (
                 create_file_executor,
             )
-            import pysqa
+            import pysqa  # noqa
 
             super().__init__(
                 executor=create_file_executor(
