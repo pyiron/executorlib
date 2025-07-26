3b76caf9836d54bbc1b909b4ad944094fcd41d3f
Jan Janßen <janssen@mpie.de>
2025-07-26 15:18:27 +0200

    extend tests

--- tests/test_standalone_batched.py
+++ tests/test_standalone_batched.py
@@ -4,7 +4,7 @@ from executorlib.standalone.batched import batched_futures
 
 
 class TestBatched(TestCase):
-    def test_check_for_available_batch(self):
+    def test_batched_futures(self):
         lst = []
         for i in list(range(10)):
             f = Future()
@@ -14,3 +14,10 @@ class TestBatched(TestCase):
         self.assertEqual(batched_futures(lst=lst, skip_lst=[[0, 1, 2]], n=3), [3, 4, 5])
         self.assertEqual(batched_futures(lst=lst, skip_lst=[[0, 1, 2], [3, 4, 5]], n=3), [6, 7, 8])
         self.assertEqual(batched_futures(lst=lst, skip_lst=[[0, 1, 2], [3, 4, 5], [6, 7, 8]], n=3), [9])
+
+    def test_batched_futures_not_finished(self):
+        lst = []
+        for _ in list(range(10)):
+            f = Future()
+            lst.append(f)
+        self.assertEqual(batched_futures(lst=lst, n=3, skip_lst=[]), [])
