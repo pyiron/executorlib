8b5f33cb0708f342d8d5cff03893971ba473b616
Jan Janßen <janssen@mpie.de>
2025-07-27 11:27:12 +0200

    fix windows tests

--- tests/test_standalone_command.py
+++ tests/test_standalone_command.py
@@ -1,3 +1,4 @@
+import os
 import importlib.util
 import sys
 import unittest
@@ -11,7 +12,7 @@ class TestCommands(unittest.TestCase):
     def test_get_interactive_execute_command_serial(self):
         output = get_interactive_execute_command(cores=1)
         self.assertEqual(output[0], sys.executable)
-        self.assertEqual(output[1].split("/")[-1], "interactive_serial.py")
+        self.assertEqual(output[1].split(os.sep)[-1], "interactive_serial.py")
 
     @unittest.skipIf(
         skip_mpi4py_test,
@@ -20,21 +21,21 @@ class TestCommands(unittest.TestCase):
     def test_get_interactive_execute_command_parallel(self):
         output = get_interactive_execute_command(cores=2)
         self.assertEqual(output[0], sys.executable)
-        self.assertEqual(output[1].split("/")[-1], "interactive_parallel.py")
+        self.assertEqual(output[1].split(os.sep)[-1], "interactive_parallel.py")
 
     def test_get_cache_execute_command_serial(self):
         file_name = "test.txt"
         output = get_cache_execute_command(cores=1, file_name=file_name)
         self.assertEqual(output[0], sys.executable)
-        self.assertEqual(output[1].split("/")[-1], "cache_serial.py")
+        self.assertEqual(output[1].split(os.sep)[-1], "cache_serial.py")
         self.assertEqual(output[2], file_name)
         output = get_cache_execute_command(cores=1, file_name=file_name, backend="slurm")
         self.assertEqual(output[0], sys.executable)
-        self.assertEqual(output[1].split("/")[-1], "cache_serial.py")
+        self.assertEqual(output[1].split(os.sep)[-1], "cache_serial.py")
         self.assertEqual(output[2], file_name)
         output = get_cache_execute_command(cores=1, file_name=file_name, backend="flux")
         self.assertEqual(output[0], sys.executable)
-        self.assertEqual(output[1].split("/")[-1], "cache_serial.py")
+        self.assertEqual(output[1].split(os.sep)[-1], "cache_serial.py")
         self.assertEqual(output[2], file_name)
 
     @unittest.skipIf(
@@ -48,14 +49,14 @@ class TestCommands(unittest.TestCase):
         self.assertEqual(output[1], "-n")
         self.assertEqual(output[2], str(2))
         self.assertEqual(output[3], sys.executable)
-        self.assertEqual(output[4].split("/")[-1], "cache_parallel.py")
+        self.assertEqual(output[4].split(os.sep)[-1], "cache_parallel.py")
         self.assertEqual(output[5], file_name)
         output = get_cache_execute_command(cores=2, file_name=file_name, backend="slurm")
         self.assertEqual(output[0], "srun")
         self.assertEqual(output[1], "-n")
         self.assertEqual(output[2], str(2))
         self.assertEqual(output[3], sys.executable)
-        self.assertEqual(output[4].split("/")[-1], "cache_parallel.py")
+        self.assertEqual(output[4].split(os.sep)[-1], "cache_parallel.py")
         self.assertEqual(output[5], file_name)
         output = get_cache_execute_command(cores=2, file_name=file_name, backend="flux")
         self.assertEqual(output[0], "flux")
@@ -63,7 +64,7 @@ class TestCommands(unittest.TestCase):
         self.assertEqual(output[2], "-n")
         self.assertEqual(output[3], str(2))
         self.assertEqual(output[4], sys.executable)
-        self.assertEqual(output[5].split("/")[-1], "cache_parallel.py")
+        self.assertEqual(output[5].split(os.sep)[-1], "cache_parallel.py")
         self.assertEqual(output[6], file_name)
         with self.assertRaises(ValueError):
             get_cache_execute_command(cores=2, file_name=file_name, backend="test")
