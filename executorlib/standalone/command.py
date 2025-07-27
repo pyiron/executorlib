1b3eb6210a20a6c9bfeaa849b08a8d48340374e9
Jan Janßen <janssen@mpie.de>
2025-07-27 10:53:39 +0200

    file executor fix parallel execution

--- executorlib/standalone/command.py
+++ executorlib/standalone/command.py
@@ -1,6 +1,7 @@
 import importlib.util
 import os
 import sys
+from typing import Optional
 
 
 def get_command_path(executable: str) -> str:
@@ -16,24 +17,40 @@ def get_command_path(executable: str) -> str:
     return os.path.abspath(os.path.join(__file__, "..", "..", "backend", executable))
 
 
-def get_cache_execute_command(file_name: str, cores: int = 1) -> list:
+def get_cache_execute_command(file_name: str, cores: int = 1, backend: Optional[str] = None) -> list:
     """
     Get command to call backend as a list of two strings
 
     Args:
         file_name (str): The name of the file.
         cores (int, optional): Number of cores used to execute the task. Defaults to 1.
+        backend (str, optional): name of the backend used to spawn tasks ["slurm", "flux"].
 
     Returns:
         list[str]: List of strings containing the python executable path and the backend script to execute
     """
     command_lst = [sys.executable]
     if cores > 1 and importlib.util.find_spec("mpi4py") is not None:
-        command_lst = (
-            ["mpiexec", "-n", str(cores)]
-            + command_lst
-            + [get_command_path(executable="cache_parallel.py"), file_name]
-        )
+        if backend is None:
+            command_lst = (
+                ["mpiexec", "-n", str(cores)]
+                + command_lst
+                + [get_command_path(executable="cache_parallel.py"), file_name]
+            )
+        elif backend == "slurm":
+            command_lst = (
+                ["srun", "-n", str(cores)]
+                + command_lst
+                + [get_command_path(executable="cache_parallel.py"), file_name]
+            )
+        elif backend == "flux":
+            command_lst = (
+                ["flux", "run", "-n", str(cores)]
+                + command_lst
+                + [get_command_path(executable="cache_parallel.py"), file_name]
+            )
+        else:
+            raise ValueError("backend should be None, slurm or flux, not {}".format(backend))
     elif cores > 1:
         raise ImportError(
             "mpi4py is required for parallel calculations. Please install mpi4py."
