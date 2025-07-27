1c89a16c87d2a24c7b16dd181f5a17ec5217d862
Jan Janßen <janssen@mpie.de>
2025-07-27 18:33:51 +0200

    use slrum args

--- tests/test_slurmjobexecutor.py
+++ tests/test_slurmjobexecutor.py
@@ -27,7 +27,7 @@ def mpi_funct(i):
 )
 class TestSlurmBackend(unittest.TestCase):
     def test_slurm_executor_serial(self):
-        with SlurmJobExecutor() as exe:
+        with SlurmJobExecutor(resource_dict={"slurm_cmd_args": ["--mpi=pmi2"]}) as exe:
             fs_1 = exe.submit(calc, 1)
             fs_2 = exe.submit(calc, 2)
             self.assertEqual(fs_1.result(), 1)
@@ -38,7 +38,7 @@ class TestSlurmBackend(unittest.TestCase):
     def test_slurm_executor_parallel(self):
         with SlurmJobExecutor(
             max_cores=2,
-            resource_dict={"cores": 2},
+            resource_dict={"cores": 2, "slurm_cmd_args": ["--mpi=pmi2"]},
             block_allocation=True,
         ) as exe:
             fs_1 = exe.submit(mpi_funct, 1)
