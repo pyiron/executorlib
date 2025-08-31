dbf3e65224a681f01b213b3833f49b203b1d5ba6
Jan Janssen <janssen@mpie.de>
2025-08-31 21:55:49 +0200

    rename variable

--- executorlib/task_scheduler/interactive/blockallocation.py
+++ executorlib/task_scheduler/interactive/blockallocation.py
@@ -18,7 +18,7 @@ from executorlib.task_scheduler.interactive.shared import (
     task_done,
 )
 
-_task_schedulder_dict: dict = {}
+_interrupt_interface_bootup_dict: dict = {}
 
 
 class BlockAllocationTaskScheduler(TaskSchedulerBase):
@@ -71,7 +71,7 @@ class BlockAllocationTaskScheduler(TaskSchedulerBase):
         self._max_workers = max_workers
         self_id = id(self)
         self._self_id = self_id
-        _task_schedulder_dict[self._self_id] = False
+        _interrupt_interface_bootup_dict[self._self_id] = False
         self._set_process(
             process=[
                 Thread(
@@ -79,7 +79,7 @@ class BlockAllocationTaskScheduler(TaskSchedulerBase):
                     kwargs=executor_kwargs
                     | {
                         "worker_id": worker_id,
-                        "stop_function": lambda: _task_schedulder_dict[self_id],
+                        "stop_function": lambda: _interrupt_interface_bootup_dict[self_id],
                     },
                 )
                 for worker_id in range(self._max_workers)
@@ -171,7 +171,7 @@ class BlockAllocationTaskScheduler(TaskSchedulerBase):
             if cancel_futures:
                 cancel_items_in_queue(que=self._future_queue)
             if isinstance(self._process, list):
-                _task_schedulder_dict[self._self_id] = True  # This is a hard shutdown
+                _interrupt_interface_bootup_dict[self._self_id] = True
                 for _ in range(len(self._process)):
                     self._future_queue.put({"shutdown": True, "wait": wait})
                 if wait:
