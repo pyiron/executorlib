1e2d21c031727feddaf7d4e04cacbc50ef766ade
Jan Janssen <janssen@mpie.de>
2025-09-08 12:03:57 +0200

    no command

--- tests/test_fluxclusterexecutor.py
+++ tests/test_fluxclusterexecutor.py
@@ -183,12 +183,11 @@ class TestPysqaSpawner(unittest.TestCase):
         interface_flux._process = process_id
         self.assertFalse(interface_flux.poll())
         self.assertFalse(interface_flux._check_process_helper(command_lst=["sleep", "1"]))
-        self.assertTrue(interface_flux.poll())
 
     def test_pysqa_spawner_error(self):
         interface_flux = PysqaSpawner(backend="flux", cores=1)
         with self.assertRaises(RuntimeError):
-            interface_flux.bootup(command_lst=["--unknonwn", "1"])
+            interface_flux.bootup(command_lst=[])
 
     def test_pysqa_spawner_big(self):
         interface_flux = PysqaSpawner(backend="flux", cores=100)
