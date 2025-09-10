c7c30204436afe285c15ee055fa4646e05d01425
Jan Janßen <janssen@mpie.de>
2025-09-10 08:02:28 +0200

    fix tests

--- tests/test_standalone_split.py
+++ tests/test_standalone_split.py
@@ -1,5 +1,6 @@
 import unittest
 from executorlib import SingleNodeExecutor
+from executorlib.api import cloudpickle_register
 from executorlib.standalone.split import split
 
 
@@ -14,6 +15,7 @@ def function_with_exception(i):
 class TestSplitFuture(unittest.TestCase):
     def test_integration_base(self):
         with SingleNodeExecutor() as exe:
+            cloudpickle_register(ind=1)
             future = exe.submit(function_with_multiple_outputs, 15)
             f1, f2, f3 = split(future=future, n=3)
             self.assertEqual(f1.result(), "a")
@@ -25,6 +27,7 @@ class TestSplitFuture(unittest.TestCase):
 
     def test_integration_exception(self):
         with SingleNodeExecutor() as exe:
+            cloudpickle_register(ind=1)
             future = exe.submit(function_with_exception, 15)
             f1, f2, f3 = split(future=future, n=3)
             with self.assertRaises(RuntimeError):
