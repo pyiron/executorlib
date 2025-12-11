d73e7056523ff46ecf18fdc290c0607a16569bd9
Jan Janssen <janssen@mpie.de>
2025-12-11 17:50:45 +0100

    improve test coverage

---
+++ tests/test_standalone_serialize.py
@@ -0,0 +1,21 @@
+import unittest
+from executorlib.standalone.serialize import _get_function_name
+
+
+def my_function(a: int, b: int) -> int:
+    return a + b
+
+
+class MyClass:
+    def __call__(self, a: int, b: int) -> int:
+        return a + b
+
+
+class TestSerialization(unittest.TestCase):
+    def test_serialization(self):
+        fn = _get_function_name(fn=my_function)
+        self.assertEqual(fn, "my_function")
+        fn = _get_function_name(fn=MyClass())
+        self.assertEqual(fn, "MyClass")
+        fn = _get_function_name(fn=None)
+        self.assertEqual(fn, "fn")
