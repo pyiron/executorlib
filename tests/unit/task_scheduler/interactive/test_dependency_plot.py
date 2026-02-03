a17df2867d353db11fa6c2c902479af9ecb35f48
Jan Janssen <janssen@mpie.de>
2026-02-03 09:26:08 +0100

    fix tests

--- tests/unit/task_scheduler/interactive/test_dependency_plot.py
+++ tests/unit/task_scheduler/interactive/test_dependency_plot.py
@@ -14,8 +14,8 @@ def my_function(i):
 class TestShortObjectName(unittest.TestCase):
     def test_short_object_name(self):
         result = _short_object_name(node=[MyClass("a"), MyClass("b")])
-        self.assertEqual("['unit.task_scheduler.interactive.test_dependency_plot.MyClass()', 'unit.task_scheduler.interactive.test_dependency_plot.MyClass()']", result)
+        self.assertEqual("['MyClass()', 'MyClass()']", result)
         result = _short_object_name(node=(MyClass("a"), MyClass("b")))
-        self.assertEqual("('unit.task_scheduler.interactive.test_dependency_plot.MyClass()', 'unit.task_scheduler.interactive.test_dependency_plot.MyClass()')", result)
+        self.assertEqual("('MyClass()', 'MyClass()')", result)
         result = _short_object_name(node={"this is a very long string far too long for a dictionary key": my_function})
         self.assertEqual("{'this is a very long s...': 'my_function()'}", result)
