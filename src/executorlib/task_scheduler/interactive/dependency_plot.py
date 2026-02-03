de437cbf2ae66292f84c679d67fe5a0313997b3a
Jan Janssen <janssen@mpie.de>
2026-02-03 09:18:30 +0100

    reduce classes

--- src/executorlib/task_scheduler/interactive/dependency_plot.py
+++ src/executorlib/task_scheduler/interactive/dependency_plot.py
@@ -326,14 +326,13 @@ def _short_object_name(node):
             }
         )
     elif "object at" in node_value_str:
-        short_name = node_value_str[1:-1].split()[0] + "()"
+        short_name = node_value_str[1:-1].split()[0].split(".")[-1] + "()"
     elif "<function" in node_value_str:
         short_name = node_value_str.split()[1] + "()"
     elif "(" in node_value_str and ")" in node_value_str:
         short_name = node_value_str.split("(")[0] + "()"
+    elif len(node_value_str) > 20:
+        short_name = node_value_str[:21] + "..."
     else:
         short_name = node_value_str
-    if len(short_name) > 20:
-        return short_name[:21] + "..."
-    else:
-        return short_name
+    return short_name
