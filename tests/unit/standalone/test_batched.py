04f2983504fc575dc9de6dd72720adea58baad94
Jan Janssen <jan-janssen@users.noreply.github.com>
2026-06-11 12:29:27 +0200

    [Feature] Check which future objects are skipped and keep track of their IDs (#1014)
    * [Test] Add duplicated test for batched
    
    * Handling exceptions
    
    * revert refactor
    
    * Raise an exception when futures failed
    
    * fix
    
    * add unit test for failing futures
    
    * compare to valueerror
    
    * fixes
    
    * two futures worked two failed
    
    * order is not guranteed
    
    * Keep track of the futures
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Format black
    
    * fix type annotation
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: pyiron-runner <pyiron@mpie.de>

--- tests/unit/standalone/test_batched.py
+++ tests/unit/standalone/test_batched.py
@@ -1,27 +1,82 @@
-from unittest import TestCase
+import unittest
 from concurrent.futures import Future
-from executorlib.standalone.batched import batched_futures
+
+from executorlib.task_scheduler.interactive.dependency import batched_futures
 
 
-class TestBatched(TestCase):
+class TestBatched(unittest.TestCase):
     def test_batched_futures(self):
         lst = []
-        for i in list(range(10)):
+        for i in range(10):
             f = Future()
             f.set_result(i)
             lst.append(f)
         batched_lst = [Future(), Future(), Future()]
-        batched_lst[0].set_result([0, 1, 2])
-        batched_lst[1].set_result([3, 4, 5])
-        batched_lst[2].set_result([6, 7, 8])
-        self.assertEqual(batched_futures(lst=lst, n=3, nested_skip_lst=set()), [0, 1, 2])
-        self.assertEqual(batched_futures(lst=lst, nested_skip_lst=batched_lst[:1], n=3), [3, 4, 5])
-        self.assertEqual(batched_futures(lst=lst, nested_skip_lst=batched_lst[:2], n=3), [6, 7, 8])
-        self.assertEqual(batched_futures(lst=lst, nested_skip_lst=batched_lst, n=3), [9])
+        batched_lst[0].set_result([id(lst[0]), id(lst[1]), id(lst[2])])
+        batched_lst[1].set_result([id(lst[3]), id(lst[4]), id(lst[5])])
+        batched_lst[2].set_result([id(lst[6]), id(lst[7]), id(lst[8])])
+        success, done_lst = batched_futures(lst=lst, n=3, nested_skip_lst=set())
+        self.assertTrue(success)
+        self.assertEqual([f.result() for f in done_lst], [0, 1, 2])
+        success, done_lst = batched_futures(lst=lst, nested_skip_lst=batched_lst[:1], n=3)
+        self.assertTrue(success)
+        self.assertEqual([f.result() for f in done_lst], [3, 4, 5])
+        success, done_lst = batched_futures(lst=lst, nested_skip_lst=batched_lst[:2], n=3)
+        self.assertTrue(success)
+        self.assertEqual([f.result() for f in done_lst], [6, 7, 8])
+        success, done_lst = batched_futures(lst=lst, nested_skip_lst=batched_lst, n=3)
+        self.assertTrue(success)
+        self.assertEqual([f.result() for f in done_lst], [9])
+
+    def test_batched_futures_duplicated(self):
+        lst = []
+        for i in range(1,4):
+            for _ in range(3):
+                f = Future()
+                f.set_result(i)
+                lst.append(f)
+        batched_lst = [Future(), Future(), Future()]
+        batched_lst[0].set_result([id(lst[0]), id(lst[1]), id(lst[2])])
+        batched_lst[1].set_result([id(lst[3]), id(lst[4]), id(lst[5])])
+        batched_lst[2].set_result([id(lst[6]), id(lst[7]), id(lst[8])])
+        success, done_lst = batched_futures(lst=lst, n=3, nested_skip_lst=set())
+        self.assertTrue(success)
+        self.assertEqual([f.result() for f in done_lst], [1, 1, 1])
+        success, done_lst = batched_futures(lst=lst, nested_skip_lst=batched_lst[:1], n=3)
+        self.assertTrue(success)
+        self.assertEqual([f.result() for f in done_lst], [2, 2, 2])
+        success, done_lst = batched_futures(lst=lst, nested_skip_lst=batched_lst[:2], n=3)
+        self.assertTrue(success)
+        self.assertEqual([f.result() for f in done_lst], [3, 3, 3])
+
+    def test_batched_futures(self):
+        lst = []
+        for i in range(10):
+            f = Future()
+            if i % 3 == 0:
+                f.set_exception(ValueError(f"Error for {i}"))
+            else:
+                f.set_result(i)
+            lst.append(f)
+        batched_lst = [Future(), Future()]
+        batched_lst[0].set_result([id(lst[1]), id(lst[2]), id(lst[4])])
+        batched_lst[1].set_result([id(lst[5]), id(lst[7]), id(lst[8])])
+        success, done_lst = batched_futures(lst=lst, n=3, nested_skip_lst=set())
+        self.assertTrue(success)
+        self.assertEqual([f.result() for f in done_lst], [1, 2, 4])
+        success, done_lst = batched_futures(lst=lst, nested_skip_lst=batched_lst[:1], n=3)
+        self.assertTrue(success)
+        self.assertEqual([f.result() for f in done_lst], [5, 7, 8])
+        succss, done_lst = batched_futures(lst=lst, nested_skip_lst=batched_lst, n=3)
+        self.assertFalse(succss)
+        with self.assertRaises(ValueError):
+            raise done_lst[0].exception()
 
     def test_batched_futures_not_finished(self):
         lst = []
         for _ in list(range(10)):
             f = Future()
             lst.append(f)
-        self.assertEqual(batched_futures(lst=lst, n=3, nested_skip_lst=set()), [])
+        success, done_lst = batched_futures(lst=lst, n=3, nested_skip_lst=set())
+        self.assertTrue(success)
+        self.assertEqual(done_lst, [])
