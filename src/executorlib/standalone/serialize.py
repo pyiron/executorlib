6ec23d30ad39ade22467ca75e1328516f5297fd2
Jan Janssen <janssen@mpie.de>
2026-09-09 18:21:16 +0200

    mypy fixes

--- src/executorlib/standalone/serialize.py
+++ src/executorlib/standalone/serialize.py
@@ -34,7 +34,7 @@ def serialize_funct(
     fn_kwargs: Optional[dict] = None,
     resource_dict: Optional[dict] = None,
     cache_key: Optional[str] = None,
-) -> tuple[str, dict]:
+) -> tuple[str, dict, Optional[Exception]]:
     """
     Serialize a function and its arguments and keyword arguments into an HDF5 file.
 
@@ -56,7 +56,8 @@ def serialize_funct(
                                    overwritten by setting the cache_key.
 
     Returns:
-        Tuple[str, dict]: A tuple containing the task key and the serialized data.
+        Tuple[str, dict, Optional[Exception]]: A tuple containing the task key, the serialized data and an
+                                               exception if the serialization failed.
 
     """
     excp = None
